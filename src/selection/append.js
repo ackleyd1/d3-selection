146669b454aaba440d0bc389c817f01da6e23165
Dave Ackley <ackleyd1@msu.edu>
2021-07-12 23:46:10 -0400

    working through append

--- src/selection/append.js
+++ src/selection/append.js
@@ -2,6 +2,8 @@ import creator from "../creator.js";
 
 export default function(name) {
   var create = typeof name === "function" ? name : creator(name);
+  
+  // append creates new Selection w/ callback
   return this.select(function() {
     return this.appendChild(create.apply(this, arguments));
   });
